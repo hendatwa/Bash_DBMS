4fbd7d7e10c69592bea211a67dfb9b150e224f66
Hend Atwa <58531043+hendatwa@users.noreply.github.com>
2025-01-26 20:39:22 +0200

    Update TableMenu.sh

--- BashProject/TableMenu.sh
+++ BashProject/TableMenu.sh
@@ -5,20 +5,20 @@ db_menu() {
   while true; do
     echo "    Database Menu:
     ----------------------"
-    echo "1. Create Table"
-    echo "2. Drop Table"
-    echo "3. Insert Record"
-    echo "4. Update Record"
-    echo "5. Truncate Table"
-    echo "6. List Tables"
-    echo "7. Select"
-    echo "8. Exit"
+    echo "1) Create Table"
+    echo "2) Drop Table"
+    echo "3) Insert Record"
+    echo "4) Update Record"
+    echo "5) Truncate Table"
+    echo "6) List Tables"
+    echo "7) Select"
+    echo "8) Exit"
     read -p "Choose an option: " option
     case $option in
       1) create_table ;;
       2) drop_table ;;
-      3) echo "insert_record" ;;
-      4) echo "update_record" ;;
+      3) echo into_table ;;
+      4) update_table ;;
       5) truncate_table ;;
       6) list_tables ;;
       7) select_from_table ;;
@@ -262,4 +262,163 @@ select_from_table() {
   else
     echo "Invalid selection type. Please enter 'all' or 'record'."
   fi
+}
+insert_record() {
+  table_file="$current_database/${choice}.txt"
+  metadata_file="$current_database/${choice}_meta.txt"
+
+  if [[ ! -f "$metadata_file" ]]; then
+    echo "Metadata file does not exist."
+    return 1
+  fi
+
+  AllCol=($(head -n 1 "$metadata_file" | awk -F "," '{ for (i=1; i<=NF; i++) print $i }'))
+  colNames=()
+  colDatatype=()
+  rowData=""
+  for i in "${AllCol[@]}"; do 
+    colName=$(cut -d : -f1 <<< "$i")  
+    colType=$(cut -d : -f2 <<< "$i")  
+
+    colNames+=("$colName")
+    colDatatype+=("$colType")
+  done
+  # echo "Columns: ${colNames[@]}"
+  # echo "Data Types: ${colDatatype[@]}"
+
+  if [[ ${#colDatatype[@]} -eq ${#colNames[@]} ]]; then
+    for i in "${!colNames[@]}"; do
+      while true; do
+        read -rp "Enter ${colNames[$i]}: " data
+        if [[ $i -eq 0 ]]; then
+          if [ $(awk -F "," -v val="$data" '$1 == val { print $1 }' "$table_file"| wc -l) -gt 0 ]; then
+            echo "PK Must be unique. Try again."
+            continue
+          fi
+        fi
+        case ${colDatatype[$i]} in
+          STRING)
+            if [[ "$data" =~ ^[a-zA-Z0-9_]+$ ]]; then
+              break
+            else
+              echo "Invalid datatype. Please enter a valid string."
+            fi
+            ;;
+          INT)
+            if [[ "$data" =~ ^-?[0-9]+$ ]]; then
+              break
+            else
+              echo "Invalid datatype. Please enter a valid integer."
+            fi
+            ;;
+          FLOAT)
+            if [[ "$data" =~ ^-?[0-9]+(\.[0-9]+)?$ ]]; then
+              break
+            else
+              echo "Invalid datatype. Please enter a valid float."
+            fi
+            ;;
+          *)
+            echo "Invalid datatype specified."
+            break
+            ;;
+        esac
+      done
+      if [[ -z "$rowData" ]]; then
+        rowData="$data"
+      else
+        rowData+=",$data"
+      fi
+    done
+    echo "$rowData" >> "$table_file"
+    echo "Record successfully inserted."
+  else
+    echo "Error: The number of columns does not match the number of datatypes."
+  fi
+}
+update_table(){
+  AllTablesName=($(ls "$current_database" | grep -E '\.txt$' | sed 's/.txt$//' | grep -v '_meta'))
+  if [[ ${#AllTablesName[@]} -eq 0 ]]; then
+    echo echo "There is no Tables Created. "
+    return
+  fi
+  local PS3="TableChoice>> "
+  select choice in "${AllTablesName[@]}" "Cancel";
+  do
+    REPLY=$REPLY-1
+    if [[ $choice == "Cancel" ]]; then
+      break
+    elif [[ -n "$choice" ]]; then
+      update_record
+      break
+      else
+      echo "Invalid choice Try again."
+    fi
+  done
+
+}
+update_record() {
+  local table_file="$current_database/${choice}.txt"
+  local metadata_file="$current_database/${choice}_meta.txt"
+
+  if [[ ! -f "$table_file" ]]; then
+    echo "Table file does not exist."
+    return
+  fi
+  local AllCol=($(head -n 1 "$metadata_file" | awk -F "," '{ for (i=1; i<=NF; i++) print $i }'))
+  local colNames=()
+  local colDatatype=()
+  for col in "${AllCol[@]}"; do
+    colNames+=("$(cut -d ':' -f1 <<< "$col")")
+    colDatatype+=("$(cut -d ':' -f2 <<< "$col")")
+  done
+
+  read -rp "Enter the number of the record to update: " update_record
+  record_line=$(grep -n "^$update_record," "$table_file" | cut -d: -f1)
+
+  if [[ -z "$record_line" ]]; then
+    echo "Record $update_record not found."
+    return
+  fi
+  record=$(sed -n "${record_line}p" "$table_file")
+
+  new_record="$update_record"
+
+  # Prompt for updated values for each column except ID
+  for i in "${!colNames[@]}"; do
+    if [[ "$i" -ne 0 ]]; then
+      current_value=$(echo "$record" | cut -d',' -f$((i + 1)))
+      while true; do
+        read -rp "Enter new value for ${colNames[$i]}: " new_value
+        case ${colDatatype[$i]} in
+          STRING)
+            if [[ "$new_value" =~ ^[a-zA-Z0-9_]+$ ]]; then
+              break
+            else
+              echo "Invalid datatype. Please enter a valid string."
+            fi
+            ;;
+          INT)
+            if [[ "$new_value" =~ ^-?[0-9]+$ ]]; then
+              break
+            else
+              echo "Invalid datatype. Please enter a valid integer."
+            fi
+            ;;
+          FLOAT)
+            if [[ "$new_value" =~ ^-?[0-9]+(\.[0-9]+)?$ ]]; then
+              break
+            else
+              echo "Invalid datatype. Please enter a valid float."
+            fi
+            ;;
+        esac
+      done
+      new_record+=",$new_value"
+    fi
+  done
+
+  # Update the record in the file
+  sed -i "${record_line}s/.*/$new_record/" "$table_file"
+  echo "Record updated successfully."
 }
